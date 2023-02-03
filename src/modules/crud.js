0a1b81dd8f9546f686d2dc0353db4c2c7620af15
Andrea <andrearivas4cinfor@gmail.com>
2023-02-03 11:37:06 +0100

    Improve display functions

--- src/modules/crud.js
+++ src/modules/crud.js
@@ -15,66 +15,127 @@ const importance = document.querySelector('.important-select')
 const addForm = document.querySelector('.form');
 
 function displayTask(task) {
-  if(task.important === 'important') {
-    const html = `
-    <div class="task-container" data-task-importance="important">
-     <div class="checkbox-task">
-      <input id="${task.index}" class="checkbox" type="checkbox">
-      <p data-index="${task.index}" class="task-text">${task.title}
-      <i class="fa-solid fa-star"></i>
-      </p>
-     </div>
-    <div class="all-icons">
-    <p date-index="${task.index}" class="task-date">${task.dueDate}</p>
-    <i id="${task.index}" class="fa-solid fa-magnifying-glass"></i>
-      <i id="${task.index}" class="fa-regular fa-pen-to-square"></i>
-    <i id="${task.index}" class="fa-regular fa-trash-can"></i>
-    </div>`;
-    listContainer.innerHTML += html;
-  } else {
-  const html = `
-  <div class="task-container">
-     <div class="checkbox-task">
-      <input id="${task.index}" class="checkbox" type="checkbox">
-      <p data-index="${task.index}" class="task-text">${task.title}</p>
-     </div>
-    <div class="all-icons">
-    <p date-index="${task.index}" class="task-date">${task.dueDate}</p>
-    <i id="${task.index}" class="fa-solid fa-magnifying-glass"></i>
-      <i id="${task.index}" class="fa-regular fa-pen-to-square"></i>
-    <i id="${task.index}" class="fa-regular fa-trash-can"></i>
-    </div>`;
-  listContainer.innerHTML += html;
-  }
-}
+      const taskContainer = document.createElement('div');
+      taskContainer.classList.add('task-container');
+      if (task.completed) {
+        taskContainer.classList.add('completed');
+        taskContainer.dataset.taskStatus = 'completed';
+      }
+      if (task.important === "important") {
+        taskContainer.dataset.taskImportance = 'important';
+      }
+      const checkboxTask = document.createElement('div');
+      checkboxTask.classList.add('checkbox-task');
+      const checkbox = document.createElement('input');
+      checkbox.id = task.index;
+      checkbox.classList.add('checkbox');
+      checkbox.type = "checkbox";
+      checkboxTask.appendChild(checkbox);
+      const taskText = document.createElement('p');
+      taskText.dataset.index = task.index;
+      taskText.classList.add('task-text');
+      taskText.textContent = task.title;
+      if (task.important === "important") {
+        const star = document.createElement('i');
+        star.classList.add('fa-solid');
+        star.classList.add('fa-star');
+        taskText.appendChild(star);
+      }
+      checkboxTask.appendChild(taskText);
+      taskContainer.appendChild(checkboxTask);
+      const allIcons = document.createElement('div');
+      allIcons.classList.add('all-icons');
+      const taskDate = document.createElement('p');
+      taskDate.dataset.index = task.index;
+      taskDate.classList.add('task-date');
+      taskDate.textContent = task.dueDate;
+      allIcons.appendChild(taskDate);
+      const magnifyingGlass = document.createElement('i');
+      magnifyingGlass.id = task.index;
+      magnifyingGlass.classList.add('fa-solid');
+      magnifyingGlass.classList.add('fa-magnifying-glass');
+      allIcons.appendChild(magnifyingGlass);
+      const pen = document.createElement('i');
+      pen.id = task.index;
+      pen.classList.add('fa-regular');
+      pen.classList.add('fa-pen-to-square');
+      allIcons.appendChild(pen);
+      const trashCan = document.createElement('i');
+      trashCan.id = task.index;
+      trashCan.classList.add('fa-regular');
+      trashCan.classList.add('fa-trash-can');
+      allIcons.appendChild(trashCan);
+      taskContainer.appendChild(allIcons);
+      listContainer.appendChild(taskContainer);
+    }
 
 function displayTasks() {
-  titleSection.textContent = "ALL TASKS"
-  const tasks = JSON.parse(localStorage.getItem('tasks'));
-  tasks.forEach((task) => {
-      const html = `
-    <div class="task-container">
-     <div class="checkbox-task">
-      <input id="${task.index}" class="checkbox" type="checkbox">
-      <p data-index="${task.index}" class="task-text">${task.title}</p>
-     </div>
-    <div class="all-icons">
-    <p date-index="${task.index}" class="task-date">${task.dueDate}</p>
-    <i id="${task.index}" class="fa-solid fa-magnifying-glass"></i>
-      <i id="${task.index}" class="fa-regular fa-pen-to-square"></i>
-    <i id="${task.index}" class="fa-regular fa-trash-can"></i>
-    </div>`;
-    listContainer.innerHTML += html;
-    }); 
+    titleSection.textContent = "ALL TASKS"
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.forEach((task) => {
+      const taskContainer = document.createElement('div');
+      taskContainer.classList.add('task-container');
+      if (task.completed) {
+        taskContainer.classList.add('completed');
+        taskContainer.dataset.taskStatus = 'completed';
+      }
+      if (task.important === "important") {
+        taskContainer.dataset.taskImportance = 'important';
+      }
+      const checkboxTask = document.createElement('div');
+      checkboxTask.classList.add('checkbox-task');
+      const checkbox = document.createElement('input');
+      checkbox.id = task.index;
+      checkbox.classList.add('checkbox');
+      checkbox.type = "checkbox";
+      checkboxTask.appendChild(checkbox);
+      const taskText = document.createElement('p');
+      taskText.dataset.index = task.index;
+      taskText.classList.add('task-text');
+      taskText.textContent = task.title;
+      if (task.important === "important") {
+        const star = document.createElement('i');
+        star.classList.add('fa-solid');
+        star.classList.add('fa-star');
+        taskText.appendChild(star);
+      }
+      checkboxTask.appendChild(taskText);
+      taskContainer.appendChild(checkboxTask);
+      const allIcons = document.createElement('div');
+      allIcons.classList.add('all-icons');
+      const taskDate = document.createElement('p');
+      taskDate.dataset.index = task.index;
+      taskDate.classList.add('task-date');
+      taskDate.textContent = task.dueDate;
+      allIcons.appendChild(taskDate);
+      const magnifyingGlass = document.createElement('i');
+      magnifyingGlass.id = task.index;
+      magnifyingGlass.classList.add('fa-solid');
+      magnifyingGlass.classList.add('fa-magnifying-glass');
+      allIcons.appendChild(magnifyingGlass);
+      const pen = document.createElement('i');
+      pen.id = task.index;
+      pen.classList.add('fa-regular');
+      pen.classList.add('fa-pen-to-square');
+      allIcons.appendChild(pen);
+      const trashCan = document.createElement('i');
+      trashCan.id = task.index;
+      trashCan.classList.add('fa-regular');
+      trashCan.classList.add('fa-trash-can');
+      allIcons.appendChild(trashCan);
+      taskContainer.appendChild(allIcons);
+      listContainer.appendChild(taskContainer);
+    });
   }
+
 
 
 function displayChecked() {
   titleSection.textContent = "COMPLETED TASKS"
   const tasks = document.querySelectorAll('.task-container');
   tasks.forEach(task => {
-    if(task.dataset.taskImportance === "true") {
-      task.style.display = 'block';
+    if(task.dataset.taskStatus === "completed") {
+      task.style.display = 'flex';
     } else {
       task.style.display = 'none';
     }
@@ -133,7 +194,7 @@ const clickHandle = (e) => {
     // -- Check completed --
     const { id } = e.target;
     const checkbox = e.target;
-    const sibling = checkbox.closest('.task-container').querySelector('.task-text');
+    const sibling = checkbox.closest('.task-container');
     if (checkbox.checked) {
       sibling.dataset.taskStatus = 'completed';
       sibling.classList.add('completed');
